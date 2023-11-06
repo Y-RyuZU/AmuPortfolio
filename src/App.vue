70b3e57af3c9594ac49b2146f00b30572a248590
Y-RyuZU <ryuzu1006@gmail.com>
2023-11-06 20:33:30 +0900

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <v-app>
+        <v-main>
+            <v-container fluid>
+
+            </v-container>
+        </v-main>
+    </v-app>
+</template>
+
+<style scoped lang="scss">
+</style>
+
+<style lang="scss">
+
+</style>
---
+++ vite.config.ts
@@ -0,0 +1,28 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import pages from 'vite-plugin-pages'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    vue(),
+    vueJsx(),
+    pages({
+      dirs: 'src/views',
+    }),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  },
+  server: {
+    host: '0.0.0.0',
+    watch: {
+      usePolling: true
+    }
+  },
+})
