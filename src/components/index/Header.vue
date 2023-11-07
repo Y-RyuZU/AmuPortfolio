97900b895f52faf5ede7c0a7794d5099911dd734
student-ops <gogakuec@gmail.com>
2023-11-08 07:23:34 +0900

    add header footer sample page

---
+++ src/components/index/Header.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <header>
+        <img src="/indexPage/top.png" alt="">
+    </header>
+</template>
---
+++ src/pages/Test.vue
@@ -0,0 +1,5 @@
+<script setup lang="ts"></script>
+
+<template>
+    <h1>test3</h1>
+</template>
