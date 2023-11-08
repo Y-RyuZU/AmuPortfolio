d19ebafe56d78bc88df420a9b9b3a16042f0c073
student-ops <gogakuec@gmail.com>
2023-11-08 10:25:03 +0900

    スタッシュからの変更をコミット

---
+++ src/components/index/CharactorFeature.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import charactorsData from '@/assets/charactors/charactors.json';
+import { ref } from 'vue/dist/vue.js';
+
+const charactor = ref(charactorsData.test);
+</script>
+
+<template>
+    <img src="/indexPage/charactor/middlebar_character.png" alt="" style="width: 100%; height: auto" />
+    <p>{{ charactor }}</p>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts">
-</script>
-
 <template>
-    <h1>home</h1>
-    <h1>home</h1>
+    <v-app>
+        <v-main>
+            <v-container class="d-flex justify-center">
+                <v-row justify="center">
+                    <v-col cols="8">
+                        <div>
+                            <PageHeader />
+                            <!-- add positon style must refact  -->
+                            <FeaturedCharactor style="position: relative; top: -10px" />
+                        </div>
+                    </v-col>
+                </v-row>
+            </v-container>
+        </v-main>
+    </v-app>
 </template>
+
+<script setup lang="ts">
+import PageHeader from '@/components/index/PageHeader.vue'
+import FeaturedCharactor from '@/components/index/CharactorFeature.vue'
+</script>
