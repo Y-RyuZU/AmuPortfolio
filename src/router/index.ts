30003805396d77693133d9707a209c467f3746a0
student-ops <gogakuec@gmail.com>
2023-11-10 12:11:17 +0900

    update router behavior

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,23 +1,18 @@
-// router/index.ts
 import { createRouter, createWebHistory } from 'vue-router';
 
-// 動的インポート用のページコンポーネントを設定
 const pageComponents = import.meta.glob('../pages/**/*.vue');
 
-// パス名を抽出してルートをマッピングするための関数を定義
 const getPathName = (path: string) => {
-    // /pages/Index.vue の場合は '/' にマッピングする
     if (path.endsWith('/Index.vue')) {
         return '/';
     }
-    // パスから /pages と .vue を除去してパス名を取得
     return path.replace(/(\.\.\/pages|\.vue$)/g, '').toLowerCase();
 };
 
-// ルートを生成
+// 既存のルートを取得
 const routes = Object.keys(pageComponents).map((path) => {
     const pathName = getPathName(path); // パス名を取得
-    // '/index' の場合は '/' にマッピングする特別な処理を行う
+
     const routePath = pathName === '/index' ? '/' : pathName;
     return {
         path: routePath,
@@ -25,10 +20,21 @@ const routes = Object.keys(pageComponents).map((path) => {
     };
 });
 
-// ルーターを作成
+routes.push({
+    path: '/datespot/:spot', // ':spot' は動的パラメータです。
+    component: () => import('../pages/DateSpot.vue')
+});
+
 const router = createRouter({
     history: createWebHistory(import.meta.env.BASE_URL),
     routes,
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
 });
 
 export default router;
